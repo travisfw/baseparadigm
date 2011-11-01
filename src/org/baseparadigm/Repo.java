9feae5735267f6aa97e3e5bd254142e5245f1e90
Travis Wellman <travis@traviswellman.com>
2011-11-01 03:10:37 -0700

    some work on subscriptions/spaces

--- src/org/baseparadigm/Repo.java
+++ src/org/baseparadigm/Repo.java
@@ -168,9 +168,7 @@ public class Repo implements Map<ContentId, byte[]>{
     public Subscription subscribe(final Pattern pattern) {
         final BlockingQueue<ContentId> q = new LinkedBlockingQueue<ContentId>();
         
-        Subscription subs = new Subscription( null // TODO what is the parent?
-                , q
-                , pattern) {
+        Subscription subs = new Subscription( pattern) {
             private boolean cancelled = false;
 
             @Override
