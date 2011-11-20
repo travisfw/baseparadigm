e06179776efd06bb0b72d3da1d8d67fd23d14381
Travis Wellman <travis@traviswellman.com>
2011-11-19 18:38:02 -0800

    MemoScriptEngine

---
+++ src/main/java/org/baseparadigm/memo/MemoScriptEngine.java
@@ -0,0 +1,99 @@
+package org.baseparadigm.memo;
+
+import java.io.Reader;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptException;
+
+public class MemoScriptEngine implements ScriptEngine{
+
+    @Override
+    public Object eval(String script, ScriptContext context)
+            throws ScriptException {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Object eval(Reader reader, ScriptContext context)
+            throws ScriptException {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Object eval(String script) throws ScriptException {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Object eval(Reader reader) throws ScriptException {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Object eval(String script, Bindings n) throws ScriptException {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Object eval(Reader reader, Bindings n) throws ScriptException {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public void put(String key, Object value) {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Object get(String key) {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Bindings getBindings(int scope) {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public void setBindings(Bindings bindings, int scope) {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public Bindings createBindings() {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public ScriptContext getContext() {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public void setContext(ScriptContext context) {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+    @Override
+    public ScriptEngineFactory getFactory() {
+        // TODO Auto-generated method stub
+        throw new Error("unimplemented");
+    }
+
+}
