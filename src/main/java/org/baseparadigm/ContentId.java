a54e64574dd7a298dd2c7ea1fa4b179274883b0d
Travis Wellman <travis@archive.org>
2011-12-22 22:33:45 -0800

    whitespace only

--- src/main/java/org/baseparadigm/ContentId.java
+++ src/main/java/org/baseparadigm/ContentId.java
@@ -24,33 +24,33 @@ public class ContentId extends BigInteger implements ToByteArray {
      *            have the length of the key length for the BaseParadigm given.
      */
     public ContentId(Repo repo, byte[] bytes) {
-	super(bytes);
-	assert repo.keyLength == bytes.length;
-	this.repo = repo;
+        super(bytes);
+        assert repo.keyLength == bytes.length;
+        this.repo = repo;
     }
 
     public ContentId(Repo repo, BigInteger key) {
-	super(key.toByteArray());
-	assert key.toByteArray().length <= repo.keyLength;
-	this.repo = repo;
+        super(key.toByteArray());
+        assert key.toByteArray().length <= repo.keyLength;
+        this.repo = repo;
     }
 
     private static boolean allZeroes(byte[] ba) {
-	for (byte b : ba)
-	    if (b != 0)
-		return false;
-	return true;
+        for (byte b : ba)
+            if (b != 0)
+                return false;
+        return true;
     }
 
     // static because the constructor uses it to call super
     // ie, the repo property on ContentID instances is not yet set
     private static byte[] pad(Repo repo, byte[] padBytes) {
-	byte[] keyBytes = new byte[repo.keyLength];
-	assert allZeroes(keyBytes) :
-	    "this java implementation fills new byte arrays with: "+ keyBytes[0];
-	System.arraycopy(padBytes, 0, keyBytes, keyBytes.length
-		- padBytes.length, padBytes.length);
-	return keyBytes;
+        byte[] keyBytes = new byte[repo.keyLength];
+        assert allZeroes(keyBytes) :
+            "this java implementation fills new byte arrays with: "+ keyBytes[0];
+        System.arraycopy(padBytes, 0, keyBytes, keyBytes.length
+                - padBytes.length, padBytes.length);
+        return keyBytes;
     }
 
     /**
@@ -59,33 +59,33 @@ public class ContentId extends BigInteger implements ToByteArray {
      */
     @Override
     public byte[] toByteArray() {
-	byte[] orig = super.toByteArray();
-	if (repo.keyLength < orig.length)
-	    throw new RuntimeException(
-		    "Somehow you obtained an id bigger than your repo supports.");
-	// there is a > 1/256 chance of padding being necessary
-	byte[] ba = pad(repo, orig);
-	assert super.equals(new BigInteger(ba));
-	return ba;
+        byte[] orig = super.toByteArray();
+        if (repo.keyLength < orig.length)
+            throw new RuntimeException(
+                    "Somehow you obtained an id bigger than your repo supports.");
+        // there is a > 1/256 chance of padding being necessary
+        byte[] ba = pad(repo, orig);
+        assert super.equals(new BigInteger(ba));
+        return ba;
     }
 
     /**
      * this.repo.get(this)
      */
     public byte[] resolve() {
-	return repo.get(this);
+        return repo.get(this);
     }
 
     @Override
     public boolean equals(Object o) {
-	if (!(o instanceof ContentId))
-	    return o instanceof BigInteger && o.equals(this);
-	return o instanceof ContentId && ((ContentId) o).repo == this.repo
-		&& super.equals(o);
+        if (!(o instanceof ContentId))
+            return o instanceof BigInteger && o.equals(this);
+        return o instanceof ContentId && ((ContentId) o).repo == this.repo
+                && super.equals(o);
     }
 
     @Override
     public ContentId clone() {
-	return new ContentId(repo, toByteArray());
+        return new ContentId(repo, toByteArray());
     }
 }
